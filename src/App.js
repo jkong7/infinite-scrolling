8e38cb40d3ea975a37ef65289c088a0c6c7a89cb
Jonathan Kong <jonathankong@jonathansmbp67.ads.northwestern.edu>
2024-11-02 15:02:36 -0500

    Infinite scrolling with last element ref

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
 import React, { useState, useRef, useCallback } from 'react'
-import useBookSearch from './useBookSearch';
+import useBookSearch from './useBookSearch'
 
-function App() {
+export default function App() {
   const [query, setQuery] = useState('')
   const [pageNumber, setPageNumber] = useState(1)
   const { loading, error, books, hasMore } = useBookSearch(query, pageNumber)
 
-  const observer = useRef()
-  const lastBookElementRef = useCallback(node => {
-    if (loading) return 
-    if (observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entires => {
-      if (entires[0].isIntersecting && hasMore) {
-        setPageNumber(prev=>prev+1)
-      }
-  })
-    if (node) observer.current.observe(node)
-  }, [loading, hasMore])
-
-  function handleSearch(e) {
+  function handleChange(e) {
     setQuery(e.target.value)
     setPageNumber(1)
   }
 
+  const observer=useRef()
+  const useLastElementRef = useCallback(node=>{
+    if (loading) return 
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries=>{
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prev=>prev+1)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
+
+
   return (
     <>
-      <input type="text" value={query} onChange={handleSearch}></input>
-      {books.map((book, index) => {
-        if (index+1===books.length) {
-          return <div key={book} ref={lastBookElementRef}>{book}</div>
+      <input type="text" value={query} onChange={handleChange}></input>
+
+      {books.map((book, index)=>{
+        if (books.length===index+1) {
+          return <div key={book} ref={useLastElementRef}>{book}</div>
         } else {
           return <div key={book}>{book}</div>
         }
       })}
+
       <div>{loading && 'Loading...'}</div>
       <div>{error && 'Error...'}</div>
     </>
-  );
+  )
 }
 
-export default App;
+
--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -1,55 +1,38 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function useBookSearch(query, pageNumber) {
-
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [books, setBooks] = useState([])
     const [hasMore, setHasMore] = useState(true)
 
-    useEffect(() => {
+    useEffect(()=>{
         setBooks([])
     }, [query])
 
-    useEffect(() => {
+    useEffect(()=>{
         setLoading(true)
         setError(false)
         let cancel 
         axios.get('http://openlibrary.org/search.json', {
             params: { q: query, page: pageNumber }, 
             cancelToken: new axios.CancelToken(c=>cancel=c)
-        })
-        .then(res => {
+        }).then(res=>{
             setBooks(prevBooks=>{
                 return [...new Set([...prevBooks, ...res.data.docs.map(b=>b.title)])]
             })
-            setHasMore(res.data.docs.length)
             setLoading(false)
+            setHasMore(res.data.docs.length)
         }).catch(e=>{
             if (axios.isCancel(e)) return 
             setError(true)
         })
         return ()=>cancel()
-    }, [query, pageNumber]); 
+    }, [query, pageNumber])
 
-    return { loading, error, books, hasMore }
+
+  return { loading, error, books, hasMore }
 }
 
-/*
-axios.get('http://openlibrary.org/search.json', {
-    params: { q: 'harry potter', page: 2 }
-});
 
-makes a request to: 
-
-http://openlibrary.org/search.json?q=harry+potter&page=2
-
-*/
-
-//Cancel token associated with each call, and useEffects clean up 
-//function calls cancel function, which runs when a new run of useEffect
-//happens (when query or pageNumber changes), this cancels the request 
-//and only the FINAL REQUEST (no new dep array change) gets called
-
-//Now custom hook: we want to return state to use 
